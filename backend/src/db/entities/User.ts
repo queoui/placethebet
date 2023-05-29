114a1d613cf0688c7ac1403f90c2fc2a18924fed
mu11ern <mu11ern@yahoo.com>
2023-05-28 21:25:07 -0700

    backend merge

---
+++ backend/src/db/entities/User.ts
@@ -0,0 +1,12 @@
+import { Entity, Property, Unique } from '@mikro-orm/core';
+import { BaseEntity } from './BaseEntity.js';
+
+@Entity({ tableName: 'users' })
+export class User extends BaseEntity {
+  @Property()
+  @Unique()
+  email!: string;
+
+  @Property()
+  name!: string;
+}
